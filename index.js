4d07e7a49d0250b158344541f3c38674c746c09d
kjais1720 <66024105+kjais1720@users.noreply.github.com>
2021-05-10 13:29:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const slides = document.querySelectorAll('.slide')
+
+var activeIndex = 0
+for (var i=0; i<slides.length; i++){
+    if (slides[i].className.slice(-6,)==='active'){
+        activeIndex=i
+    }
+}
+
+console.log(activeIndex)
+
+document.querySelectorAll(".prev").forEach((prev)=>{
+    prev.addEventListener('click', ()=>{
+        if( activeIndex === 0){
+            activeIndex = slides.length-1
+            toggle()
+        } else{
+            activeIndex-=1
+            toggle()
+        }
+    })
+})
+
+document.querySelectorAll(".next").forEach((nex)=>{
+    nex.addEventListener('click', ()=>{
+        if( activeIndex === slides.length-1){
+            activeIndex = 0
+            toggle()
+        } else{
+            activeIndex+=1
+            toggle()
+        }
+    })
+})
+
+function toggle(){
+    document.querySelector(".active").classList.remove("active")
+    slides[activeIndex].classList.add("active")
+}
